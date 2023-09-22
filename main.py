4fe18552fc9a11c807f0e5311125f57b8d292598
Isaquehg <isaqueh.goncalves@gmail.com>
2023-09-22 11:51:59 -0300

    effects added

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ output_stream = p.open(format=pyaudio.paFloat32,
 def apply_overdrive(input_data, gain=2.0):
     return input_data * gain
 
+'''
 # Chorus Effect
 def apply_chorus(input_data, depth=500, rate=1):
     num_delayed_signals = 3  # Number of delayed signals (adjust as needed)
@@ -52,7 +53,7 @@ def apply_chorus(input_data, depth=500, rate=1):
     combined_signal = np.mean([input_data] + delayed_signals, axis=0)
     
     return combined_signal
-
+'''
 # Wah Wah Effect
 def apply_wah_wah(input_data, depth=1000, rate=0.5):
     # Create a modulating wave (sine wave) for the wah-wah effect
@@ -90,6 +91,44 @@ def apply_compressor(input_data, threshold=-20, ratio=4, attack_time=10):
 
     return output_data
 
+# Equalizer Effect
+def apply_equalizer(input_data, low_gain=1.0, mid_gain=1.0, high_gain=1.0, sample_rate=44100):
+    # Design bandpass filters for low, mid, and high frequencies
+    low_cutoff = 100  # Adjust as needed
+    low_width = 100   # Adjust as needed
+    low_b = ss.firwin(31, [low_cutoff - low_width / 2, low_cutoff + low_width / 2], fs=sample_rate)
+    
+    mid_cutoff = 1000  # Adjust as needed
+    mid_width = 200    # Adjust as needed
+    mid_b = ss.firwin(31, [mid_cutoff - mid_width / 2, mid_cutoff + mid_width / 2], fs=sample_rate)
+    
+    high_cutoff = 5000  # Adjust as needed
+    high_width = 1000   # Adjust as needed
+    high_b = ss.firwin(31, [high_cutoff - high_width / 2, high_cutoff + high_width / 2], fs=sample_rate)
+    
+    # Apply the filters and adjust gains
+    low_output = ss.lfilter(low_b, 1.0, input_data) * low_gain
+    mid_output = ss.lfilter(mid_b, 1.0, input_data) * mid_gain
+    high_output = ss.lfilter(high_b, 1.0, input_data) * high_gain
+    
+    # Combine the outputs
+    output_data = low_output + mid_output + high_output
+    
+    return output_data
+
+# Volume Boost Effect
+def apply_volume_boost(input_data, gain=2.0):
+    return input_data * gain
+
+# Pitch Shift Effect
+def apply_pitch_shift(input_data, semitones=2.0, sample_rate=44100):
+    # Calculate the pitch shift factor
+    pitch_shift_factor = 2 ** (semitones / 12.0)
+    
+    # Resample the input audio
+    output_data = ss.resample(input_data, int(len(input_data) / pitch_shift_factor))
+    
+    return output_data
 
 # Main loop
 try:
@@ -97,18 +136,24 @@ try:
     while True:
         # Real time audio input
         input_data = np.frombuffer(input_stream.read(chunk_size), dtype=np.float32)
+        print(input_data)
+        print(type(input_data))
+        print(input_data.shape)
+        print(input_data[1])
 
         # Applying effects
         processed_data = apply_compressor(input_data)
         #processed_data = apply_overdrive(processed_data)
         #processed_data = apply_wah_wah(processed_data)
-        processed_data = apply_chorus(processed_data)
+        #processed_data = apply_equalizer(processed_data)
+        #processed_data = apply_volume_boost(processed_data)
+        processed_data = apply_pitch_shift(processed_data)
 
         # Received audio output
         output_stream.write(processed_data.tobytes())
 
 except KeyboardInterrupt:
-    print("Pedaleira de guitarra encerrada.")
+    print("Effects interface stopped.")
 
 finally:
     # Close stream
