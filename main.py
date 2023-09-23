dfd41da9fa8b1b3479e4b417e0c5b397dd3190b1
Isaquehg <isaqueh.goncalves@gmail.com>
2023-09-23 18:07:54 -0300

    effects modified

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from matplotlib import pyplot as plt
 import pyaudio
 import numpy as np
 import scipy.signal as ss
+import scipy.interpolate as si
+import sounddevice as sd
 
 # PyAudio basic config
 n_channels_input = 1
@@ -12,10 +15,12 @@ output_device = 0
 
 # PyAudio object
 p = pyaudio.PyAudio()
+'''
 for i in range(p.get_device_count()):
     info = p.get_device_info_by_index(i)
     print(f"Device {i}: {info['name']}")
     print((info['name'], info['maxInputChannels']))
+'''
 
 # Opening Audio Input Stream with the built-in microphone
 input_stream = p.open(format=pyaudio.paFloat32,
@@ -29,47 +34,27 @@ output_stream = p.open(format=pyaudio.paFloat32,
                        rate=sample_rate,
                        output=True)
 
+duration = 5  # Duration of the noise-only recording in seconds
 
-# Overdrive Effect
-def apply_overdrive(input_data, gain=2.0):
-    return input_data * gain
+# Record the noise-only segment
+print("Recording noise profile...")
+noise_profile = sd.rec(int(sample_rate * duration), samplerate=sample_rate, channels=1)
+sd.wait()  # Wait for the recording to complete
 
-'''
-# Chorus Effect
-def apply_chorus(input_data, depth=500, rate=1):
-    num_delayed_signals = 3  # Number of delayed signals (adjust as needed)
-    max_delay = int(depth * sample_rate / 1000)  # Maximum delay in samples
-    
-    # Initialize the delayed signals
-    delayed_signals = [np.zeros_like(input_data) for _ in range(num_delayed_signals)]
-    
-    # Apply modulation to each delayed signal
-    for i in range(num_delayed_signals):
-        delay = max_delay - (i * max_delay // (num_delayed_signals - 1))
-        modulator = np.sin(2 * np.pi * rate * np.arange(len(input_data)))
-        delayed_signals[i] = np.roll(input_data, delay) * modulator
-        
-    # Combine the original and delayed signals
-    combined_signal = np.mean([input_data] + delayed_signals, axis=0)
-    
-    return combined_signal
-'''
-# Wah Wah Effect
-def apply_wah_wah(input_data, depth=1000, rate=0.5):
-    # Create a modulating wave (sine wave) for the wah-wah effect
-    modulator = np.sin(2 * np.pi * rate * np.arange(len(input_data)))
+# Convert to a NumPy array
+original_noise_profile = noise_profile[:, 0]
 
-    # Create a bandpass filter
-    cutoff = depth * modulator + 1000  # Adjust the center frequency and depth as needed
-    nyquist = 0.5 * sample_rate
-    width = 0.1  # Width of the bandpass filter (adjust as needed)
-    order = 21  # Filter order (adjust as needed)
-    b = ss.firwin(order, [cutoff[0] - 0.5 * width, cutoff[0] + 0.5 * width], nyq=nyquist)
-    
-    # Apply the filter to the input data
-    wah_wah_data = ss.lfilter(b, 1.0, input_data)
-    
-    return wah_wah_data
+desired_length = 1024
+
+# Calculate the downsampling factor
+downsampling_factor = len(original_noise_profile) // desired_length
+
+# Downsample the noise profile by taking every nth sample
+downsampled_noise_profile = original_noise_profile[::downsampling_factor]
+
+# Ensure the resulting noise profile has the desired length
+downsampled_noise_profile = downsampled_noise_profile[:desired_length]
+
 
 # Compressor Effect
 def apply_compressor(input_data, threshold=-20, ratio=4, attack_time=10):
@@ -92,43 +77,81 @@ def apply_compressor(input_data, threshold=-20, ratio=4, attack_time=10):
     return output_data
 
 # Equalizer Effect
-def apply_equalizer(input_data, low_gain=1.0, mid_gain=1.0, high_gain=1.0, sample_rate=44100):
-    # Design bandpass filters for low, mid, and high frequencies
-    low_cutoff = 100  # Adjust as needed
-    low_width = 100   # Adjust as needed
-    low_b = ss.firwin(31, [low_cutoff - low_width / 2, low_cutoff + low_width / 2], fs=sample_rate)
+def apply_equalizer(input_audio, low_gain=1.0, mid_gain=1.0, high_gain=1.0, sample_rate=44100):
+    # Define filter parameters for each band (frequency range, bandwidth, and order)
+    low_freq_range = [20, 200]  # Adjust as needed
+    mid_freq_range = [200, 2000]  # Adjust as needed
+    high_freq_range = [2000, 20000]  # Adjust as needed
     
-    mid_cutoff = 1000  # Adjust as needed
-    mid_width = 200    # Adjust as needed
-    mid_b = ss.firwin(31, [mid_cutoff - mid_width / 2, mid_cutoff + mid_width / 2], fs=sample_rate)
+    # Design bandpass filters for each band
+    low_b = ss.butter(4, [f / (sample_rate / 2) for f in low_freq_range], btype='band')
+    mid_b = ss.butter(4, [f / (sample_rate / 2) for f in mid_freq_range], btype='band')
+    high_b = ss.butter(4, [f / (sample_rate / 2) for f in high_freq_range], btype='band')
     
-    high_cutoff = 5000  # Adjust as needed
-    high_width = 1000   # Adjust as needed
-    high_b = ss.firwin(31, [high_cutoff - high_width / 2, high_cutoff + high_width / 2], fs=sample_rate)
+    # Apply the filters to the input audio
+    low_output = ss.lfilter(low_b[0], low_b[1], input_audio) * low_gain
+    mid_output = ss.lfilter(mid_b[0], mid_b[1], input_audio) * mid_gain
+    high_output = ss.lfilter(high_b[0], high_b[1], input_audio) * high_gain
     
-    # Apply the filters and adjust gains
-    low_output = ss.lfilter(low_b, 1.0, input_data) * low_gain
-    mid_output = ss.lfilter(mid_b, 1.0, input_data) * mid_gain
-    high_output = ss.lfilter(high_b, 1.0, input_data) * high_gain
+    # Combine the outputs to create the equalized audio
+    equalized_audio = low_output + mid_output + high_output
     
-    # Combine the outputs
-    output_data = low_output + mid_output + high_output
-    
-    return output_data
+    return equalized_audio
 
 # Volume Boost Effect
-def apply_volume_boost(input_data, gain=2.0):
+def apply_volume_boost(input_data, gain=1000.0):
     return input_data * gain
 
+# Distortion Effect
+def apply_distortion(input_audio, gain=100.0):
+    # Rectify the audio signal
+    rectified_signal = np.abs(input_audio)
+
+    # Amplify the rectified signal
+    distorted_signal = rectified_signal * gain
+
+    return distorted_signal
+
 # Pitch Shift Effect
-def apply_pitch_shift(input_data, semitones=2.0, sample_rate=44100):
+def apply_pitch_shift(input_data, desired_length=1024):
     # Calculate the pitch shift factor
-    pitch_shift_factor = 2 ** (semitones / 12.0)
     
-    # Resample the input audio
-    output_data = ss.resample(input_data, int(len(input_data) / pitch_shift_factor))
+    # Calculate the desired output length
+    output_length = int(desired_length)
+    
+    # Create a time array for the input data
+    input_time = np.arange(len(input_data))
+    
+    # Create a time array for the output data
+    output_time = np.arange(output_length) * (len(input_data) / output_length)
+    
+    # Use interpolation to resample the input audio to the desired length
+    interpolator = si.interp1d(input_time, input_data, kind='linear', fill_value="extrapolate")
+    output_data = interpolator(output_time)
     
     return output_data
+
+def apply_low_pass_filter(input_audio, cutoff_frequency=3000, sample_rate=44100, order=4):
+    # Calculate the Nyquist frequency
+    nyquist = 0.5 * sample_rate
+
+    # Calculate the normalized cutoff frequency
+    normalized_cutoff = cutoff_frequency / nyquist
+
+    # Design the low-pass filter
+    b, a = ss.butter(order, normalized_cutoff, btype='low')
+
+    # Apply the filter to the input audio
+    filtered_audio = ss.lfilter(b, a, input_audio)
+
+    return filtered_audio
+
+# Calculating the average signal
+received = 0
+frame_size = 1024  # Adjust the frame size as needed
+
+# Initialize an array to store the mean values
+mean_signal = np.zeros(frame_size)
 
 # Main loop
 try:
@@ -136,18 +159,23 @@ try:
     while True:
         # Real time audio input
         input_data = np.frombuffer(input_stream.read(chunk_size), dtype=np.float32)
-        print(input_data)
-        print(type(input_data))
-        print(input_data.shape)
-        print(input_data[1])
+        print(input_data[-1])
+        #print(type(input_data))
+        #print(input_data.shape)
+        #print(len(input_data))
+        #print(input_data[1])
 
         # Applying effects
         processed_data = apply_compressor(input_data)
-        #processed_data = apply_overdrive(processed_data)
-        #processed_data = apply_wah_wah(processed_data)
-        #processed_data = apply_equalizer(processed_data)
-        #processed_data = apply_volume_boost(processed_data)
-        processed_data = apply_pitch_shift(processed_data)
+        processed_data = apply_equalizer(processed_data)
+        processed_data = apply_volume_boost(processed_data)
+        #processed_data = apply_pitch_shift(processed_data)
+        #processed_data = apply_low_pass_filter(input_data)
+        #processed_data = apply_distortion(input_data)
+        
+        # Average Signal
+        mean_signal += processed_data
+        received += 1.0
 
         # Received audio output
         output_stream.write(processed_data.tobytes())
@@ -156,9 +184,39 @@ except KeyboardInterrupt:
     print("Effects interface stopped.")
 
 finally:
-    # Close stream
     input_stream.stop_stream()
     input_stream.close()
     output_stream.stop_stream()
     output_stream.close()
     p.terminate()
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(mean_signal * 1.0)
+    plt.show()
+
+    # Perform FFT
+    fft_result = np.fft.fft(processed_data)
+
+    # Calculate the magnitude spectrum
+    magnitude_spectrum = np.abs(fft_result)
+
+    # Convert frequency bins to Hertz
+    n = len(processed_data)
+    frequencies = np.fft.fftfreq(n, 1.0 / sample_rate)
+
+    # Set the frequency range to zoom in (adjust as needed)
+    start_freq = 1000  # Start frequency (Hz)
+    end_freq = 13000  # End frequency (Hz)
+
+    # Find the corresponding indices in the frequency array
+    start_index = np.argmax(frequencies >= start_freq)
+    end_index = np.argmax(frequencies >= end_freq)
+
+    # Plot the magnitude spectrum with zoomed-in x-axis
+    plt.figure(figsize=(10, 6))
+    plt.plot(frequencies[start_index:end_index], magnitude_spectrum[start_index:end_index])
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Magnitude')
+    plt.title('Zoomed-In Frequency Spectrum of Audio Signal')
+    plt.grid()
+    plt.show()
